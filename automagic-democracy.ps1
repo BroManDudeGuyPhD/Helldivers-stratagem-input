e1aae9020628fb982095045f512461d4f0360b74
BroManDudeGuyPhD <andrew.fossier@gmail.com>
2024-06-02 00:21:08 -0500

    Now utilizing aliases, so in addition to the full names simply "Autocannon" can be passed to the script. Also added and interactive terminal to search for stratagems as well as update then search for certain codes as an easy way to check the JSON file

--- automagic-democracy.ps1
+++ automagic-democracy.ps1
@@ -1,17 +1,53 @@
 <#
-    Code by BroManDudeGuyPhD
-    Purpose: Spread Democracy more effectively with this strategem keypress tool.
-    5/16/2024
+
+Code by BroManDudeGuyPhD
+Purpose: Spread Democracy more effectively with this strategem keypress tool.
+Initiated 5/16/2024
+
+.SYNOPSIS
+Automate keypresses for Helldivers 2 Stratagems using an automaticlly generated list of stratagem codes
+
+.EXAMPLE
+automagic-democracy -update
+automagic-democracy -strat "Airstrike"
+powershell.exe -File "C:\pathToScript\automagic-democracy.ps1" -strat "Airstrike"
+
+.PARAMETER strat
+The literal stratagem name, like "Airstrike". Names can be found in stratagems.json
+Some names are like AC-8 Autocannon. Alias trims that to "Autocannon", and the strat entered is also checked against the alias value, so either the full literal name or shortened alias value can be passed to the script
+
+.PARAMETER update
+Fetches paramater names and codes from online Wiki
+
+.PARAMETER terminal
+Opens up an interactive terminal
+
+.OUTPUTS
+Will create stratagems.json if not already present in home directory. 
+This is parsed data from the Helldivers 2 wiki at https://helldivers.fandom.com/wiki/Stratagem_Codes_(Helldivers_2 
+
+.NOTES
+README.md has more information, but access the Repo for the most info at https://github.com/BroManDudeGuyPhD/Helldivers-stratagem-input
+
+.LINK
+https://github.com/BroManDudeGuyPhD/Helldivers-stratagem-input
 #>
 
+
 param (
+    [Alias("stratagem","code", "strategem")]
     [Parameter(Mandatory = $false)]
     [string]$strat,
+
     [Parameter(Mandatory = $false)]
-    [switch]$update
+    [switch]$update,
+
+    [Alias("test","testing", "help")]
+    [Parameter(Mandatory = $false)]
+    [switch]$terminal
 )
 
-#wait time in milliseconds between keypresses
+# Wait time in milliseconds between keypresses
 $keypressWaitTime = 70
 $stratagesmDataFile = "stratagems.json"
 
@@ -19,15 +55,14 @@ Add-Type -AssemblyName microsoft.VisualBasic
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName PresentationCore,PresentationFramework
 
-#Right now the stratagem list is hardcoded, but I intend to generate it dynamically by scraping data off a Wiki page and storing that in the stratagems.json file
-
 function Update-Json{
 	Write-Host "Updating Stratagems"
 
     $StratagemJson= @{}
 	$StratagemList = New-Object System.Collections.ArrayList
     
-    #Copy-Item .\stratagems.json stratagems.json.bak
+    # At some point I want to implement customizable aliases for stratagems, this is a backup I've commented out for now since only the raw data is stored
+    # Copy-Item .\stratagems.json stratagems.json.bak
     $wikiURL = "https://helldivers.fandom.com/wiki/Stratagem_Codes_(Helldivers_2)"
 
     $request = Invoke-WebRequest -Uri $wikiURL
@@ -43,7 +78,7 @@ function Update-Json{
             
             if ($cells[0].tagName -eq "TD") {
                 $cells[2].childNodes | Foreach-Object {
-					#Looks in the table cell that contains stratagem codes for images, and uses image name to determine direction
+					# Looks in the table cell that contains stratagem codes for images, and uses image name to determine direction
                     if ($_.className -eq 'image') { 
                         $arrowDirection = $_
                         $arrowDirection = $arrowDirection.href
@@ -66,24 +101,51 @@ function Update-Json{
                     }
                 }
 
-				#Parse the HTML to determine info for each Stratagem
-
+				# Parse the HTML to determine info for each Stratagem
                 $name = $cells[1] | ForEach-Object { ("" + $_.InnerText).Trim() }
                 $coolDown = $cells[3] | ForEach-Object { ("" + $_.InnerText).Trim() }
                 $uses = $cells[4] | ForEach-Object { ("" + $_.InnerText).Trim() }
                 $activationTime = $cells[5] | ForEach-Object { ("" + $_.InnerText).Trim() }
-				#$activationTime = @($cells[5] | ForEach-Object { ("" + $_.InnerText).Trim() })
-				
-				#This may not be super important, but, doing a bit of formatting on the strings
-				$name = $name.Replace('"',"")
+				# $activationTime = @($cells[5] | ForEach-Object { ("" + $_.InnerText).Trim() })
+
+                # Stratagem Name formatting
+                # Some names are like AC-8 Autocannon. Alias trims that to "Autocannon"
+                # However, "Eagle Rearm" will not have a unique alias value
+                $name = $name.Replace('"',"")
+
+
+                #$name = "AC-8 Autocannon"
+                if ($name.Contains("-")) {
+                    $shortName = $name -split '-', 2;
+                    $alias = $shortName[1] -split ' ', 2;
+                    $alias = $alias[1..($alias.Length-1)]
+                    $alias
+                }
+
+                # If there is no need for an alias, just fill in alias falue with name to prevent search issues
+                else {
+                    $alias = $name;
+                    $name
+                }
+
+
+                # Cooldown & Activation Time formatting
+                # Removes minute conversion, leaving only refferences to seconds
 				$coolDown = $coolDown.Replace("seconds","").Replace("second","")
+                $cooolDownSeconds = $coolDown.Split("(");
+                $cooolDownSeconds = $cooolDownSeconds[0].Trim();
+
 				$activationTime = $activationTime.Replace("seconds","").Replace("second","")
-				
+				$activationTimeSeconds = $activationTime.Split("("); 
+                $activationTimeSeconds = $activationTimeSeconds[0].Trim();
+
+                # Adding all the parsed and formatted data to a List
                 $StratagemList.Add(@{"Name"=$name;
-				"Alias"="";"Code"=$inputCode;
-				"Cooldown"=$coolDown;
-				"Uses"=$uses;
-				"ActivationTime"=$activationTime;})
+				"Alias"= "$alias";
+                "Code"= $inputCode;
+				"Cooldown"= $cooolDownSeconds;
+				"Uses"= $uses;
+				"ActivationTime"= $activationTimeSeconds;})
 
                 continue
             }
@@ -96,23 +158,33 @@ function Update-Json{
 
     foreach ($key in $StratagemJson.Democracy.Stratagems) {
 		
-        if($key.Name -eq $strat){
+        if($key.Name -eq $strat -Or $key.Alias -eq $strat){
             Write-Host $key.Name $key.Code -ForegroundColor Cyan
         }
 		
     }
 }
-if ($update) {
-	Update-Json
-} 
 
 
+Function Learn-Keypress($value){
 try {
 	# This is there the script reads stratagems.json to check for the codes. If the file is missing, you get a popup
 	# The datafile does NOT have to be generated this way, the -update command can be used when running the script
-	$stratagemCodeFile = (Get-Content $stratagesmDataFile -Raw -ErrorAction Stop) | ConvertFrom-Json
-	$requestedCode = $stratagemCodeFile.Democracy.Stratagems | Where-Object { $_.Name -eq $strat }
-	$code = $requestedCode.code
+	
+    $value = $value.Replace('"',"") # Removes any quotes from parameter input
+    $stratagemCodeFile = (Get-Content $stratagesmDataFile -Raw -ErrorAction Stop) | ConvertFrom-Json
+	$requestedCode = $stratagemCodeFile.Democracy.Stratagems | Where-Object { $_.Name -eq $value -or $_.Alias -eq $value }
+
+    if($null -eq $requestedCode){
+        $requestedCode = $stratagemCodeFile.Democracy.Stratagems | Where-Object { $_.Name.contains($value) -or $_.Alias.contains($value) }
+
+        if($null -eq $requestedCode){
+            return "No Stratagems matching $($value)"
+        }
+        return $requestedCode
+    }
+
+	return $requestedCode.code
 }
 catch [System.Management.Automation.ItemNotFoundException] {
 	$ButtonType = [System.Windows.MessageBoxButton]::YesNoCancel
@@ -127,9 +199,10 @@ catch [System.Management.Automation.ItemNotFoundException] {
 	}
 
 }
+}
 
-#Output the code for troubleshooting
-#$code
+# Output the code for troubleshooting
+# $code
 function Enter-Keypress {
     param (
         [string[]]$stratagemCode
@@ -137,11 +210,11 @@ function Enter-Keypress {
         
     $keyPresses = $stratagemCode.ToCharArray();
 
-    #Strategem Keypress type setting in-game MUST be "press" for this function to work. I could not get holding down CTRL to work
-    [System.Windows.Forms.SendKeys]::SendWait("^"); #This is the CTRL Key Press
+    # Strategem Keypress type setting in-game MUST be "press" for this function to work. I could not get holding down CTRL to work
+    [System.Windows.Forms.SendKeys]::SendWait("^"); # This is the CTRL Key Press
     
     foreach ($key in $keyPresses) {
-        #Loops thru each character of the codes entered manually above, presses the corresponding arrow, then waits 50 ms
+        # Loops thru each character of the codes entered manually above, presses the corresponding arrow, then waits 50 ms
         Start-Sleep -Milliseconds $keypressWaitTime
         if ($key -eq "U") {
             [System.Windows.Forms.SendKeys]::SendWait("{UP}");
@@ -158,8 +231,106 @@ function Enter-Keypress {
     }
 }
 
+function Get-Funky{
+    # Written by Matt
+    # https://stackoverflow.com/questions/35022078/how-do-i-output-ascii-art-to-console
+    param([string]$Text)
+
+    # Use a random colour for each character
+    $Text.ToCharArray() | ForEach-Object{
+        switch -Regex ($_){
+            # Ignore new line characters
+            "`r"{
+                break
+            }
+            # Start a new line
+            "`n"{
+                Write-Host " ";break
+            }
+            # Use random colours for displaying this non-space character
+            "[^ ]"{
+                # Splat the colours to write-host
+                $writeHostOptions = @{
+                    ForegroundColor = ([system.enum]::GetValues([system.consolecolor])) | get-random
+                    # BackgroundColor = ([system.enum]::GetValues([system.consolecolor])) | get-random
+                    NoNewLine = $true
+                }
+                Write-Host $_ @writeHostOptions
+                break
+            }
+            " "{Write-Host " " -NoNewline}
+
+        } 
+    }
+}
+
+function Show-Menu {
+    Write-Host  " "
+    Write-Host "Testing Menu"
+    Write-Host "1: Search Stratagems" -ForegroundColor Cyan
+    Write-Host "2: UPDATE Stratagems" -ForegroundColor Red
+    Write-Host "3: Stratagem Hero$([char]174) *COMING SOON*" -ForegroundColor Magenta
+    Write-Host "(Q)uit or (H)elp"
+    Write-Host  " "
+}
+
+
+
+function terminal() {
+    [System.Console]::Clear()
+    Write-Host "===============================================================================================================" -ForegroundColor DarkBlue
+    Write-Host @'
+     $$$$$$\              $$\                       $$$$$$\    $$\                         $$\     
+    $$  __$$\             $$ |                     $$  __$$\   $$ |                        $$ |    
+    $$ /  $$ |$$\   $$\ $$$$$$\    $$$$$$\         $$ /  \__|$$$$$$\    $$$$$$\  $$$$$$\ $$$$$$\   
+    $$$$$$$$ |$$ |  $$ |\_$$  _|  $$  __$$\ $$$$$$\\$$$$$$\  \_$$  _|  $$  __$$\ \____$$\\_$$  _|  
+    $$  __$$ |$$ |  $$ |  $$ |    $$ /  $$ |\______|\____$$\   $$ |    $$ |  \__|$$$$$$$ | $$ |    
+    $$ |  $$ |$$ |  $$ |  $$ |$$\ $$ |  $$ |       $$\   $$ |  $$ |$$\ $$ |     $$  __$$ | $$ |$$\ 
+    $$ |  $$ |\$$$$$$  |  \$$$$  |\$$$$$$  |       \$$$$$$  |  \$$$$  |$$ |     \$$$$$$$ | \$$$$  |
+    \__|  \__| \______/    \____/  \______/         \______/    \____/ \__|      \_______|  \____/  version 1.1                                                                                                                                                                                                                                                                                                  
+'@ -ForegroundColor Magenta
+
+    Write-Host "                                           Aiding Democracy since 2024" -ForegroundColor Cyan
+    Write-Host "===============================================================================================================" -ForegroundColor DarkBlue
+
+    do {
+        Show-Menu
+        $selection = Read-Host "Please make a selection"
+        switch ($selection) {
+            '1' {
+                $enteredCode = Read-Host "Stratagem Name"
+                $code = Learn-Keypress -value $enteredCode
+                $code
+            } '2' {
+                Update-Json
+            } '3' {
+                'Coming soon Helldiver... go spread Democracy in the meantime'
+            }'h'{
+                'If you are having trouble getting started, check out https://github.com/BroManDudeGuyPhD/Helldivers-stratagem-input'
+            }
+        }
+        pause
+    }
+    until ($selection -eq 'q')
+
+
+}
+
 
 # Main
-if ($null -ne $code) {
-    Enter-Keypress -stratagemCode $code
+
+if ($update) {
+	Update-Json
+} 
+
+if ($terminal){
+    terminal
+}
+
+if ($strat) {
+    $code = Learn-Keypress -value $strat
+    $code
+    if($code) {
+        Enter-Keypress -stratagemCode $code
+    }
 }
